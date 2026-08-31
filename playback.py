61cf475dbd77800a953a40f248798dabeba09bb9
Kevin Maldonado <kevin.maldonado@qro.s-riko.com>
2026-08-31 16:25:50 -0600

    playback functions

--- playback.py
+++ playback.py
@@ -1,3 +1,4 @@
+
 import os
 import glob
 import shlex
@@ -13,3 +14,19 @@ PLAYBACK_PLAYER_PATH = resolve_path(SETTINGS["PLAYBACK_PLAYER_PATH"])
 PLAYBACK_PLAYER_ARGS = SETTINGS["PLAYBACK_PLAYER_ARGS"]
 PLAYBACK_PROCESS_NAME = SETTINGS["PLAYBACK_PROCESS_NAME"]
 PLAYBACK_TIMEOUT_SECONDS = SETTINGS["PLAYBACK_TIMEOUT_SECONDS"]
+
+def get_video_files():
+    search_pattern = os.path.join(PLAYBACK_VIDEO_DIR,PLAYBACK_VIDEO_PATTERN)
+    files_list = glob.glob(search_pattern)
+    final_list = []
+
+    for file in files_list:
+        if os.path.isfile(file):
+            final_list.append(file)
+    
+    final_list.sort(key=os.path.getmtime, reverse=True)
+            
+    return final_list
+
+def build_playlist(video_files):
+    pass
